10492009d535c58d79ee84688dca085dca14f6ff
ZOAmple <gerwin.zimpel1999@gmail.com>
2026-02-19 22:25:49 -0300

    Added some comments

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Series = A Pandas 1-Dimensional labeled aray that can hold any data type
+#          Like a single column in excel or like an appartment floor with many rooms
+
+
+# DataFrame = A tabular data structure with rows and Columns
+
+# Importing files
+
+# Selection
+
+# Filtering
+
+# Aggregation
+
+# Data Cleaning
